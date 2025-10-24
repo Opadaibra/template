9fe87950a9ce7fe8929ea65410e6333b376f74c5
o.ebraheem <o.ebraheem@smartsoft-sy.net>
2025-10-24 18:43:07 +0300

    update

--- src/app/views/auth/confirm-otp/confirm-otp.component.ts
+++ src/app/views/auth/confirm-otp/confirm-otp.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NgIf } from "../../../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
+import { PermissionDirective } from "../../../core/directives/permission.directive";
 
 @Component({
   selector: 'app-confirm-otp',
-  imports: [],
+  imports: [NgIf, PermissionDirective],
   templateUrl: './confirm-otp.component.html',
   styleUrl: './confirm-otp.component.scss'
 })
